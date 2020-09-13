528ecc2cb43517413871620d70cf5c32cc4c8de4
Martijn Dorsman <martijn.dorsman@gmail.com>
2020-09-13 18:09:49 +0200

    Implement Pest testing

--- tests/Feature/QuoteOfTheDayCommandTest.php
+++ tests/Feature/QuoteOfTheDayCommandTest.php
@@ -3,106 +3,87 @@
 namespace Tests\Feature;
 
 use Illuminate\Support\Facades\Http;
-use Tests\TestCase;
 
-class QuoteOfTheDayCommandTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testQuoteOfTheDayCommand()
-    {
-        $author = 'Veronica A. Shoffstall';
-        $quoteId = 1;
-        $body = 'Simplicity is the ultimate sophistication.';
-
-        Http::fake([
-            config('app.api_url') . '/qotd' => Http::response([
-                'quote' => [
-                    'id' => $quoteId,
-                    'author' => $author,
-                    'body' => $body,
-                ]
-            ]),
-
-            config('app.api_url') . "/quotes/$quoteId/fav" => Http::response([
+test('quote of the day command works', function () {
+    $author = 'Veronica A. Shoffstall';
+    $quoteId = 1;
+    $body = 'Simplicity is the ultimate sophistication.';
+    Http::fake([
+        config('app.api_url') . '/qotd' => Http::response([
+            'quote' => [
                 'id' => $quoteId,
                 'author' => $author,
                 'body' => $body,
-                'user_details' => [
-                    'favorite' => true
-                ]
-            ])
-        ]);
+            ]
+        ]),
 
-        $this->artisan('qod')
-            ->expectsOutput($body)
-            ->expectsOutput("- $author")
-            ->expectsQuestion('Do you like this quote?', 'Yes')
-            ->assertExitCode(0);
-    }
+        config('app.api_url') . "/quotes/$quoteId/fav" => Http::response([
+            'id' => $quoteId,
+            'author' => $author,
+            'body' => $body,
+            'user_details' => [
+                'favorite' => true
+            ]
+        ])
+    ]);
+    $this->artisan('qod')
+        ->expectsOutput($body)
+        ->expectsOutput("- $author")
+        ->expectsQuestion('Do you like this quote?', 'Yes')
+        ->assertExitCode(0);
+});
 
-    public function testIfQuoteIsNotFound()
-    {
-        $author = 'Veronica A. Shoffstall';
-        $quoteId = 1;
-        $body = 'Simplicity is the ultimate sophistication.';
-
-        Http::fake([
-            config('app.api_url') . '/qotd' => Http::response([
-                'quote' => [
-                    'id' => $quoteId,
-                    'author' => $author,
-                    'body' => $body,
-                ]
-            ]),
-
-            config('app.api_url') . "/quotes/$quoteId/fav" => Http::response([
-                "error_code" => 40,
-                "message" => "Quote not found."
-            ])
-        ]);
-
-        $this->artisan('qod')
-            ->expectsOutput($body)
-            ->expectsOutput("- $author")
-            ->expectsQuestion('Do you like this quote?', 'Yes')
-            ->expectsOutput('Quote not found.')
-            ->assertExitCode(1);
-    }
-
-    public function testIfQuoteIsNotFavorited()
-    {
-        $author = 'Veronica A. Shoffstall';
-        $quoteId = 1;
-        $body = 'Simplicity is the ultimate sophistication.';
-
-        Http::fake([
-            config('app.api_url') . '/qotd' => Http::response([
-                'quote' => [
-                    'id' => $quoteId,
-                    'author' => $author,
-                    'body' => $body,
-                ]
-            ]),
-
-            config('app.api_url') . "/quotes/$quoteId/fav" => Http::response([
+test('if quote is not found', function () {
+    $author = 'Veronica A. Shoffstall';
+    $quoteId = 1;
+    $body = 'Simplicity is the ultimate sophistication.';
+    Http::fake([
+        config('app.api_url') . '/qotd' => Http::response([
+            'quote' => [
                 'id' => $quoteId,
                 'author' => $author,
                 'body' => $body,
-                'user_details' => [
-                    'favorite' => false
-                ]
-            ])
-        ]);
+            ]
+        ]),
 
-        $this->artisan('qod')
-            ->expectsOutput($body)
-            ->expectsOutput("- $author")
-            ->expectsQuestion('Do you like this quote?', 'Yes')
-            ->expectsOutput('Could not favorite the quote.')
-            ->assertExitCode(1);
-    }
-}
+        config('app.api_url') . "/quotes/$quoteId/fav" => Http::response([
+            "error_code" => 40,
+            "message" => "Quote not found."
+        ])
+    ]);
+    $this->artisan('qod')
+        ->expectsOutput($body)
+        ->expectsOutput("- $author")
+        ->expectsQuestion('Do you like this quote?', 'Yes')
+        ->expectsOutput('Quote not found.')
+        ->assertExitCode(1);
+});
+test('if quote is not favorited', function () {
+    $author = 'Veronica A. Shoffstall';
+    $quoteId = 1;
+    $body = 'Simplicity is the ultimate sophistication.';
+    Http::fake([
+        config('app.api_url') . '/qotd' => Http::response([
+            'quote' => [
+                'id' => $quoteId,
+                'author' => $author,
+                'body' => $body,
+            ]
+        ]),
+
+        config('app.api_url') . "/quotes/$quoteId/fav" => Http::response([
+            'id' => $quoteId,
+            'author' => $author,
+            'body' => $body,
+            'user_details' => [
+                'favorite' => false
+            ]
+        ])
+    ]);
+    $this->artisan('qod')
+        ->expectsOutput($body)
+        ->expectsOutput("- $author")
+        ->expectsQuestion('Do you like this quote?', 'Yes')
+        ->expectsOutput('Could not favorite the quote.')
+        ->assertExitCode(1);
+});
---
+++ tests/Pest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tests;
+
+use LaravelZero\Framework\Testing\TestCase;
+
+uses(CreatesApplication::class, TestCase::class)->in(__DIR__);
